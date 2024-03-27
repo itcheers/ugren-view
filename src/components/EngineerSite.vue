cfeec2162e5763fd4e98676a4e11c3c201accf75
Ace <wow.itsme.ace@gmail.com>
2024-03-27 17:31:48 +0800

    update

--- src/components/EngineerSite.vue
+++ src/components/EngineerSite.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue'
+import { reactive, ref, watch } from 'vue'
 
 const hasTable = ref('no')
 const changeTableState = (state) => {
@@ -8,6 +8,35 @@ const changeTableState = (state) => {
   else
     hasTable.value = 'no'
 }
+
+const filledColNumber = ref(0);
+const blankColNumber = ref(0);
+
+const filledInputs = reactive([]);
+const generateFilledInputs = () => {
+  if (filledColNumber.value > filledInputs.length) {
+    for (let i = filledInputs.length; i < filledColNumber.value; i++) {
+      filledInputs.push({ title: `Title ${ i + 1 }`, value: '' });
+    }
+  } else if (filledColNumber.value < filledInputs.length) {
+    filledInputs.splice(filledColNumber.value);
+  }
+};
+
+const blankInputs = reactive([]);
+const generateBlankInputs = () => {
+  if (blankColNumber.value > blankInputs.length) {
+    for (let i = blankInputs.length; i < blankColNumber.value; i++) {
+      blankInputs.push({ title: `Title ${ filledInputs.length + i + 1 }`, value: '' });
+    }
+  } else if (blankColNumber.value < blankInputs.length) {
+    blankInputs.splice(blankColNumber.value);
+  }
+};
+
+const rowNumber = ref(0);
+watch(filledColNumber, generateFilledInputs);
+watch(blankColNumber, generateBlankInputs);
 
 </script>
 
@@ -23,7 +52,7 @@ const changeTableState = (state) => {
         <input type="text" id="machineModel" placeholder="Enter machine/model name">
       </div>
       <div>
-        <label>Column Type: </label>
+        <label>Purpose: </label>
         <input type="radio" id="test" name="columnType" value="Test">
         <label for="test">Test </label>
         <input type="radio" id="debug" name="columnType" value="Debug">
@@ -40,17 +69,47 @@ const changeTableState = (state) => {
       </div>
       <div>
         <label for="table">Table: </label>
-        <input type="checkbox" id="table" @click="changeTableState($event.target.checked)"/>
+        <input type="checkbox" id="table" @click="changeTableState($event.target.checked)" />
 
         <div v-if="hasTable === 'yes'">
           <label for="filledCol">Filled Column Number: </label>
-          <input type="number" id="filledCol" />
+          <input type="number" id="filledCol" v-model="filledColNumber" @input="generateFilledInputs" />
+          <div v-if="filledColNumber > 0">
+            <div v-for="(input, index) in filledInputs" :key="index">
+              <label :for="'col' + index">{{ 'Title ' + (index + 1) + ' ' }}</label>
+              <input type="text" :id="'col' + index" v-model="input.value">
+            </div>
+          </div>
           <br />
+
           <label for="blankCol">Blank Column Number: </label>
-          <input type="number" id="blankCol" />
+          <input type="number" id="blankCol" v-model="blankColNumber" @input="generateBlankInputs" />
+          <div v-if="blankColNumber > 0">
+            <div v-for="(input, index) in blankInputs" :key="index">
+              <label :for="'col' + index + filledColNumber">{{ 'Title ' + (index + 1 + filledColNumber) + ' ' }}</label>
+              <input type="text" :id="'col' + index + filledColNumber" v-model="input.value">
+            </div>
+          </div>
           <br />
-          <label for="row">Row Number: </label>
-          <input type="number" id="row" />
+
+          <label for="rowNumber">Row Number: </label>
+          <input type="number" id="rowNumber" v-model="rowNumber" />
+          <br />
+
+          <table>
+            <thead>
+            <tr>
+              <th v-for="input in filledInputs">{{ input.value }}</th>
+              <th v-for="input in blankInputs">{{ input.value }}</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="row in rowNumber">
+              <td v-for="x in filledColNumber">FILLED {{ x }}</td>
+              <td v-for="x in blankColNumber"></td>
+            </tr>
+            </tbody>
+          </table>
         </div>
       </div>
 
@@ -67,5 +126,11 @@ const changeTableState = (state) => {
 textarea {
   width: 100%;
   height: 100px;
+}
+
+input[type="number"]::-webkit-inner-spin-button,
+input[type="number"]::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
 }
 </style>
--- src/components/UserSite.vue
+++ src/components/UserSite.vue
@@ -1,14 +1,22 @@
 <script setup>
 
-const requirements = [
-    'download',
-    'install',
-    'development',
-    'test',
-    'deploy'
+const name = "DL68 printer";
+const purpose = "TEST";
+const stepNumber = 1;
+
+const requirements = "this is requirement..."
+
+const tableTitles = [
+  "serial number",
+  "filled 1",
+  "filled 2",
+  "blank 1",
+  "blank 2",
 ]
 
 const totalRows = 8;
+const filledCols = 2;
+const blankCols = 2;
 
 </script>
 
@@ -17,9 +25,21 @@ const totalRows = 8;
     USER SITE
   </h1>
   <div>
+    <h2>{{ name }} for {{ purpose }} step {{ stepNumber }} </h2>
+
     <h2>requirements: </h2>
-    <p v-for="(requirement, index) in requirements">{{ index }}.{{ requirement }}</p>
+    <p>{{ requirements }}</p>
     <h2>records: </h2>
+    <table>
+      <tr>
+        <th v-for="title in tableTitles">{{ title }}</th>
+      </tr>
+      <tr v-for="serial_no in totalRows">
+        <td>SERIAL No. {{ serial_no }}</td>
+        <td v-for="index in filledCols">FILLED</td>
+        <td v-for="index in blankCols"></td>
+      </tr>
+    </table>
     <h2>picture: </h2>
   </div>
 </template>
